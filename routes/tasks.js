cdf8e51fc91ad7c0c4f4c9c94bd73601dfc223e7
dejijaye <dejijaye@gmail.com>
2016-10-18 19:08:21 +0100

    add connection with mongodb

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://dejijaye:nutty@ds033086.mlab.com:33086/mean2do', ['tasks'])
 
+// Get All Tasks
 router.get('/tasks', function(req, res, next) {
-    res.send('Task Api');
+    db.tasks.find(function(err, tasks) {
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    })
+});
+
+// Get Single Tasks
+router.get('/task/:id', function(req, res, next) {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)} ,function(err, task) {
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    })
 });
 
 module.exports = router;
